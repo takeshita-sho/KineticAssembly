3f22164c18e316c1ca6b138a85b763dbb10e0216
takeshita-sho <takeshita.sho@outlook.com>
2024-11-22 17:24:19 -0500

    More performant - has some unnecessary imports atm

--- KineticAssemblyJ/optim.jl
+++ KineticAssemblyJ/optim.jl
@@ -5,9 +5,17 @@ using OptimizationOptimisers
 using ForwardDiff
 using Zygote
 using Flux
+using OrderedCollections
 using SciMLSensitivity
 using OptimizationOptimJL
+using SciMLStructures: Tunable, replace, replace!
+using SymbolicIndexingInterface: parameter_values
 include("./ReactionNetwork.jl")
+#parameters()
+
+#using SymbolicIndexingInterface
+#pgetter = getp(odeprob) #have to insert symbols for rates
+#psetter = setp(odeprob, [:a, :b, :c])
 
 # TODO: Try using lower lr
 # TODO: Try using forwarddiff and reversediff
@@ -16,26 +24,54 @@ function optim(rn,tspan,p_init,monomer_conc,lr,iters,AD,integrator;verbose=false
     pmap = Catalyst.paramsmap(rn)
     pkeys = collect(keys(pmap))
     k_symbols = sort(pkeys, by = k -> pmap[k])
-    init_rates = get_rates(p_init,k_symbols)
-    u0 = get_species_conc(monomer_conc,rn)
     
+    #I think I need to feed in the pmap along with the unorganized key array so
+    # I can return rates in the same order as the keys - there will be duplicate rates
+    #println("getting rates")
+    #flush(stdout)
+    init_rates = get_rates(p_init,k_symbols)
+    #println("rates gotten")
+    #println("getting species conc")
+    #flush(stdout)
+    u0 = get_species_conc(monomer_conc,rn)
+    #println("species conc gotten")
+    #flush(stdout)
     if verbose
         println(init_rates)
     end
-    prob = ODEProblem(rn, u0, tspan, init_rates)
-
+    #println("Constructing ODE with jacobian")
+    #flush(stdout)
+    #println("u0: ",u0)
+    #println("Equations: ",ModelingToolkit.get_eqs(rn))
+    #println("Unknown: ",ModelingToolkit.get_unknowns(rn))
+    prob = ODEProblem(rn, u0, tspan, init_rates; jac = true)
+    #println("prob.u0", prob.u0)
+    #println("prob.p", prob.p)
+    #println("ODE constructed")
+    #flush(stdout)
     #get total yield from end of simulation for loss
     function loss(p::AbstractVector{T}, _) where T
         #Get params from rn struc -> p gives updated params
 
         #David calculated koff from kon, but changes C0, which shouldnt be done
-        
         #p is a vector of duals
-        
+        #println("Loss being calculated")
         rates = get_rates(p,k_symbols)
         #println(rates)
         #Remaking ode with updated parameters
-        newprob = remake(prob; p=rates)
+        # newprob = remake(prob; p=rates)
+        #no paramsmap, params, p_init,parameters
+        #println(prob.p)
+        #println(parameters(rn).p)
+        #newprob = remake(prob;p=rates,u0=u0)#replace(Tunable(), prob.p, rates))#p=copy(rates),u0=copy(prob.u0))#
+        newprob = remake(prob;p=rates)#replace(Tunable(), prob.p, values(rates)))
+        #println("p values: ",newprob.p)
+        #println("rates: ",rates)
+        #replace!(Tunable(), newprob.p, rates)#newprob.ps, rates)#does this need to be a vector?
+
+        # Set new parameters
+        #psetter(newprob, rates)
+
 
         #need to save at least at beginning and final time point to get final yield
         sol = solve(newprob, integrator; saveat=[tspan[2]], abstol=1e-10, reltol=1e-8, maxiters=1e7)#, dtmin=1e-12, force_dtmin=true)
@@ -58,21 +94,32 @@ function optim(rn,tspan,p_init,monomer_conc,lr,iters,AD,integrator;verbose=false
         #println(Sys.total_memory())
         #peak_memory_bytes = Sys.maxrss()
         #println("Peak memory usage so far: $(peak_memory_bytes / (1024^2)) MB")
+        #println("Loss $(loss)")
+        #flush(stdout)
         return loss
 
     end
     
+    #println("constructing optimization function")
+    #flush(stdout)
     #optf = OptimizationFunction(loss, Optimization.AutoZygote())
     optf = OptimizationFunction(loss, AD)
+    #println("optf constructed")
+    #println("Constructing optproblem")
+    #flush(stdout)
     optprob = OptimizationProblem(optf, p_init)
-    
+    #println("optp constructed")
+    #flush(stdout)
     # TODO: Use various built in optimizers 
     # This does not do integration, just Optimization
     # This does not work if I give it an integration algorithm - so not taking derivative
     
     #sol = solve(optprob, Optimization.LBFGS(); maxiters=iters)
+    #println("Solving")
+    #flush(stdout)
     sol = solve(optprob, OptimizationOptimisers.Adam(lr); maxiters=iters)
-
+    #println("Solved")
+    #flush(stdout)
     return get_rates(sol.u, k_symbols)
 end
 end
@@ -81,16 +128,17 @@ end
 #For fully connected m should be amount of monomers in each species
 #I think C0 should be a different value maybe
 function get_rates(forward_rates::Vector{T},k_symbols; delta_G_kb_T::Float64=-20., C0::Float64=1e6) where T
-    rates = Vector{T}()
+    rates = Dict{Num,T}(k_symbols .=> zeros(Float64, length(k_symbols)))
+    #rates = Vector{T}(undef, 2 * length(forward_rates))
     for (m,k_on) in enumerate(forward_rates)
-        #push!(rates, k_on)
-        #push!(rates, k_on * C0 * exp(m * delta_G_kb_T))
+        rates[k_symbols[2m - 1]] = k_on
+        rates[k_symbols[2m]] = k_on * C0 * exp(m * delta_G_kb_T)
 
         # This is more inefficient, but inplace modifications are not allowed with AutoZygote
-        rates = [rates; [k_on, k_on * C0 * exp(m * delta_G_kb_T)]]
+        #rates = [rates; [k_on, k_on * C0 * exp(m * delta_G_kb_T)]]
     end
 
-    return Dict(zip(k_symbols,rates))
+    return rates#Dict(zip(k_symbols,rates))#rates
 end
 
 #Fix this - probably indexing needs to use funsym(symbol)
@@ -99,8 +147,7 @@ function get_species_conc(monomers,rn)
     mon_spec = [funcsym(Symbol("X",i),t) for i in 1:length(monomers)]
     #n = length(monomers)
     species = Catalyst.get_species(rn)
-    
-    conc_dict = Dict{}()
+    conc_dict = Dict{Num,Float64}()
     for (i,spec) in enumerate(mon_spec)
         conc_dict[spec] = monomers[i]
     end
